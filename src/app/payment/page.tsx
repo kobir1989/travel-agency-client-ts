9eb5483d5c6939a571993b1f8a25db44d867162d
kobir1989 <kobir.h.ritu@gmail.com>
2023-08-11 16:30:44 +0600

    feat: add payment page layout

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+const PaymentPage = () => {
+  return (
+    <main>
+      <Container maxWidth="lg" sx={{ mt: '8rem', mb: '5rem' }}>
+        Payment Page
+      </Container>
+    </main>
+  );
+};
+
+export default PaymentPage;
